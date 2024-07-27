eb41c5bd208cad6e125ae48de7fb49073a47ff8e
Ahmed Alkabir <korabekaali@outlook.com>
2024-07-27 21:40:05 +0200

    fix NoneType status

--- orange-pi/yolov8_npu/yolov8_npu.py
+++ orange-pi/yolov8_npu/yolov8_npu.py
@@ -282,7 +282,9 @@ class YOLOv8NPU:
     
 
     def plot(self, results):
-        return self._draw(image=self._img_to_plot, boxes=results[0], classes=results[1], scores=results[2])
+        if results[0]:
+            return self._draw(image=self._img_to_plot, boxes=results[0], classes=results[1], scores=results[2])
+        return self._img_to_plot
     
 
     def __call__(self, frame):
