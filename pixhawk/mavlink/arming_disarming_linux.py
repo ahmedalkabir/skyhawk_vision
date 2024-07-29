f37114082d6a107e1861cc85c213a43c24d89e94
Ahmed Alkabir <korabekaali@outlook.com>
2024-07-29 12:10:28 +0200

    add mavlink scripts

---
+++ pixhawk/mavlink/arming_disarming_linux.py
@@ -0,0 +1,37 @@
+from pymavlink import mavutil
+import time
+
+master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
+
+# Wait a heartbeat before sending commands
+master.wait_heartbeat()
+
+# https://mavlink.io/en/messages/common.html#MAV_CMD_COMPONENT_ARM_DISARM
+
+# Arm 
+# master.arducopter_arm() or:
+master.mav.command_long_send(
+    master.target_system,
+    master.target_component,
+    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+    0,
+    1, 0, 0, 0, 0, 0, 0)
+
+# wait until arming confirmed (can manually check with master.motors_armed())
+print("Waiting for the vehicle to arm")
+master.motors_armed_wait()
+print('Armed!')
+
+time.sleep(5)
+
+# Disarm
+# master.arducopter_disarm() or:
+master.mav.command_long_send(
+    master.target_system,
+    master.target_component,
+    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+    0,
+    0, 0, 0, 0, 0, 0, 0)
+
+# wait until disarming confirmed
+master.motors_disarmed_wait()
